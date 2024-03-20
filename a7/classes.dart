99638ed6fb0ba94eb68a3930ab30ff800360a9c4
Adriano Medeiros <123388123+AdrianoMedeirosDev@users.noreply.github.com>
2024-03-20 14:11:54 -0300

    Add files via upload

---
+++ a7/classes.dart
@@ -0,0 +1,12 @@
+void main() {
+
+    // Instanciando um carro 
+    Carro carro = Carro("ABC1234", 123456);
+
+    // Instanciando um animal 
+    Animal animal = Animal(1.5, "Marrom");
+
+    // Instanciando uma consulta 
+    Consulta consulta = Consulta(DateTime.now(), "João", "Dr. Silva");
+
+}
